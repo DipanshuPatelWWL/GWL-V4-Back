7c82f76660f2228ab13be8536cf929d77c075efd
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-06-24 13:16:08 +0530

    update port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const path=require("path")
 const dotenv=require("dotenv");
 
 dotenv.config();
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 4000;
 
 //database connect
 database.connect();
